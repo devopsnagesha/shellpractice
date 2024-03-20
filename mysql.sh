6034ec405c8553aabd4600c8a0bac9b23a2d758c
nagesha technology <asbconstruction19@gmail.com>
2024-03-20 20:53:54 +0530

    Syntex

--- mysql.sh
+++ mysql.sh
@@ -16,9 +16,9 @@ syatemctl enable mysqld &>>$LOG
 systemctl start mysqld &>>$LOG
 check_status $?
 
-Print_Task_Heading "Setup MySQL Password"
+print_task_heading "Setup MySQL Password"
  echo 'show databases' |mysql -h mysql-dev.angadicnc.online -uroot -p${mysql_root_password} &>>$LOG
  if [ $? -ne 0 ]; then
    mysql_secure_installation --set-root-pass ${mysql_root_password} &>>$LOG
  fi
- Check_Status $?
+check_status $?
